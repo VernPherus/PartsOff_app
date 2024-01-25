5f681ba1410bbd1c2ab5aa2255be463014a1bb0b
VernPherus <vernpherus@gmail.com>
2024-01-25 19:02:54 +0800

    Added view binding and navigation resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -33,6 +37,7 @@ android {
 }
 
 dependencies {
+    val navVersion = "2.7.6"
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -41,4 +46,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
 }
