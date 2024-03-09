bed46a8051d81a2016f56d9e0b8d6b41e4a0fc7b
VernPherus <vernpherus@gmail.com>
2024-03-09 10:45:42 +0800

    fixed ProductView

--- app/src/main/java/com/dreamdevs/partsoff_app/ProductView.kt
+++ app/src/main/java/com/dreamdevs/partsoff_app/ProductView.kt
@@ -1,5 +1,6 @@
 package com.dreamdevs.partsoff_app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dreamdevs.partsoff_app.databinding.ActivityProductViewBinding
@@ -8,6 +9,7 @@ class ProductView : AppCompatActivity() {
 
     private lateinit var binding : ActivityProductViewBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductViewBinding.inflate(layoutInflater)
@@ -15,7 +17,13 @@ class ProductView : AppCompatActivity() {
 
         val bundle : Bundle?= intent.extras
         val title = bundle!!.getString("title")
+        val desc = bundle.getString("description")
+        val price = bundle.getString("price")
+        val qty = bundle.getString("qty")
 
         binding.productTitle.text = title
+        binding.productDesc.text = desc
+        binding.itemPrice.text = "₱ $price"
+        binding.itemQty.text = qty
     }
 }
