9d4ebfb915bae89ba0a50cb8a56855af7af209a7
VernPherus <vernpherus@gmail.com>
2024-03-10 11:56:54 +0800

    added user profile

--- app/src/main/java/com/dreamdevs/partsoff_app/CheckoutActivity.kt
+++ app/src/main/java/com/dreamdevs/partsoff_app/CheckoutActivity.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.dreamdevs.partsoff_app.databinding.ActivityCheckoutBinding
 
 class CheckoutActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityCheckoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityCheckoutBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_checkout)
+        setContentView(binding.root)
 
         val buttonConfirmOrder: Button = findViewById(R.id.buttonConfirmOrder)
         buttonConfirmOrder.setOnClickListener {
             Toast.makeText(this, "Order Confirmed!", Toast.LENGTH_LONG).show()
             finish()
         }
+
+
     }
 }
